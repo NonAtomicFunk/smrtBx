ef49fac9a5fab253a217e68a2a6e56601a0fb038
admin2 <admin2@macbook-pro-admin2.local>
2019-01-03 16:19:03 +0200

    codable

--- smrtBx/Model/DataModel.swift
+++ smrtBx/Model/DataModel.swift
@@ -18,4 +18,44 @@ struct DataModel: Codable {
     let bigImage: String
     let latitude: Double
     let longitude: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case type
+        case title
+        case shortDescription
+        case description
+        case smallImage
+        case bigImage
+        case latitude
+        case longitude
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id, forKey: .id)
+        try container.encode(type, forKey: .type)
+        try container.encode(title, forKey: .title)
+        try container.encode(shortDescription, forKey: .shortDescription)
+        try container.encode(description, forKey: .description)
+        try container.encode(smallImage, forKey: .smallImage)
+        try container.encode(bigImage, forKey: .smallImage)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try container.decode(String.self, forKey: .id)
+        self.type = try container.decode(String.self, forKey: .type)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.shortDescription = try container.decode(String.self, forKey: .shortDescription)
+        self.description = try container.decode(String.self, forKey: .description)
+        self.bigImage = try container.decode(String.self, forKey: .description)
+        self.smallImage = try container.decode(String.self, forKey: .smallImage)
+        self.latitude = try container.decode(Double.self, forKey: .latitude)
+        self.longitude = try container.decode(Double.self, forKey: .longitude)
+    }
 }
