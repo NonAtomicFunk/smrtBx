3d2cd4f359e685b7abd187c714f2a31a0c769c21
admin2 <admin2@macbook-pro-admin2.local>
2019-01-03 15:37:45 +0200

    Initial Commit

---
+++ smrtBx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smrtBx
+//
+//  Created by admin2 on 1/3/19.
+//  Copyright © 2019 Alexander Berezovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
